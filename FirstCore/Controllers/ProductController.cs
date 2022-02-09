f96f7c3f44f575529e5f8c27ae1ba39c0776c93e
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-09 10:36:51 +0300

    Add project files.

---
+++ FirstCore/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using FirstCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DatabaseContext _db;
+            public ProductController(DatabaseContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Product> pList = _db.Products.ToList();
+            return View(pList);
+        }
+    }
+}
---
+++ FirstCore/Migrations/20220209072730_EkleProductTableNew.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstCore.Migrations
+{
+    public partial class EkleProductTableNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
